b8237d98737407e7150c38dc5a52fac0bf570bdb
dubaokun <dubaokun@xiaomi.com>
2019-04-09 13:31:10 +0800

    add minstack

--- 020-顺时针打印矩阵/spiralMatrix.js
+++ 020-顺时针打印矩阵/spiralMatrix.js
@@ -1,3 +1,30 @@
-function spiralMatrix(array){
-    
+function spiralMatrix(matrix){
+    let row = matrix.length
+    if(row==0)return []
+    let col = matrix[0].length
+    let rowMin=0,colMin=0,rowMax =row-1,colMax=col-1
+    let count = 0;max_count = row*col
+    let res = []
+    while(count<max_count){
+        for(let i = colMin;i<=colMax;i++){
+            res[count++]=matrix[rowMin][i]
+        }
+        if(count == size) break; 
+        for(let i =rowMin+1;i<=rowMax;i++ ){
+            res[count++]=matrix[i][colMax]
+        }
+        if(count == size) break; 
+        for(let i =colMax-1;i>=colMin;i-- ){
+            res[count++]=matrix[rowMax][i]
+        }
+       for(let i = rowMax-1;i>rowMin;i--){
+        res[count++]=matrix[i][colMin]
+       }
+
+      rowMin++
+      rowMax--
+      colMax--
+      colMin++
+    }
+    return res
 }
---
+++ 021-包含min函数的栈/minStack.js
@@ -0,0 +1,36 @@
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    
+};
+
