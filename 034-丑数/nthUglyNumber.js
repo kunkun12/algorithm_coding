8d47a7c237436373687386075cfa3857b1dc6e01
dubaokun <dubaokun@xiaomi.com>
2019-04-10 20:43:57 +0800

    update ugly number

--- 034-丑数/nthUglyNumber.js
+++ 034-丑数/nthUglyNumber.js
@@ -2,7 +2,7 @@ function nthUglyNumber(n){
     let index2=0
     let index3=0
     let index5=0
-    let index=1
+    let index=0
     while(index<n){
         let val = Math.min(ugly[index2]*2,ugly[index3]*3,ugly[index5]*5)
         if (val == ugly[index2] * 2) {
@@ -15,7 +15,7 @@ function nthUglyNumber(n){
             ++index5;
         }
 
-        ugly[index++] = val;
+        ugly[++index] = val;
     }
     let result = ugly[n - 1];
     return result;
