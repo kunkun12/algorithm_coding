1ba4735329a5be531c6a8b21246c6a4e0e0db485
dubaokun <dubaokun@xiaomi.com>
2019-04-10 20:40:38 +0800

    uglynumber

---
+++ 034-丑数/nthUglyNumber.js
@@ -0,0 +1,22 @@
+function nthUglyNumber(n){
+    let index2=0
+    let index3=0
+    let index5=0
+    let index=1
+    while(index<n){
+        let val = Math.min(ugly[index2]*2,ugly[index3]*3,ugly[index5]*5)
+        if (val == ugly[index2] * 2) {
+            ++index2;
+        }
+        if (val == ugly[index3] * 3) {
+            ++index3;
+        }
+        if (val == ugly[index5] * 5){
+            ++index5;
+        }
+
+        ugly[index++] = val;
+    }
+    let result = ugly[n - 1];
+    return result;
+}
